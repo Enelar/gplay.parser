1b3bb7b317a3fb2676d398c09306453f47172fcf
Kirill Berezin <enelar@develop-project.ru>
2015-02-04 02:04:40 +0000

    Minimal submodules includes

---
+++ rpc.php
@@ -0,0 +1,31 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors','On');
+
+include_once('phpsql/phpsql.php');
+include_once('phpsql/pgsql.php');
+$sql = new phpsql();
+$pg = $sql->Connect("pgsql://postgres@localhost/gplay");
+include_once('phpsql/wrapper.php');
+
+include_once('phpsql/db.php');
+db::Bind(new phpsql\utils\wrapper($pg));
+
+function phoxy_conf()
+{
+  $ret = phoxy_default_conf();
+
+  return $ret;
+}
+
+function default_addons( $name )
+{
+  $ret =
+  [
+    "cache" => "no",
+    "result" => "canvas",
+  ];
+  return $ret;
+}
+
+include('phoxy/index.php');
